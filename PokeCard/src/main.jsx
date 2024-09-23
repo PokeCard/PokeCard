c0ed2211a11901ef6f6ba9e0d662f3b5fd97a60b
challenge-repository <julioandarestu80@gmail.com>
2024-09-23 23:42:23 +0700

    PokeCard - Add Router

--- PokeCard/src/main.jsx
+++ PokeCard/src/main.jsx
@@ -1,11 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+// import App from "./App.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
 // import './index.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/catch",
+    element: <Catch />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
---
+++ PokeCard/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <>
+      <h1 className="display-2">Test Bootstrap</h1>
+    </>
+  );
+}
+
+export default Home;
