6ddf540dc001e399110ddca9346ac627fc9ff7e4
challenge-repository <julioandarestu80@gmail.com>
2024-09-24 22:32:41 +0700

    Adding contents to card

--- PokeCard/src/components/CardPokemon.jsx
+++ PokeCard/src/components/CardPokemon.jsx
@@ -5,10 +5,9 @@ function CardPokemon() {
       <Card style={{ width: "18rem" }}>
         <Card.Img variant="top" src="holder.js/100px180" />
         <Card.Body>
-          <Card.Title>Card Title</Card.Title>
+          <Card.Title>Pikachu</Card.Title>
           <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
+            Memiliki kekuatan untuk mengeluarkan petir kepada lawannya.
           </Card.Text>
         </Card.Body>
       </Card>
